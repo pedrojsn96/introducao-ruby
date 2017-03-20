a8280a9a46d07cb328684c7e029e0430fca03d29
pedrojsn96 <pjsn@cin.ufpe.br>
2017-03-20 16:55:28 +0000

    Add value nos fields de um FORM

--- introducao-crawler/form.rb
+++ introducao-crawler/form.rb
@@ -2,7 +2,35 @@ require 'mechanize'
 
 agent = Mechanize.new 
 
+# Vê como quebrar o captacha -> tentar fazer isso agora de tarde
+# Clicar no Button
+# Pegar o resultado da pesquisa
+# Listar os 10 primeiros resultados
+
+
+agent.get("http://srv01.tjpe.jus.br/consultaprocessualunificada/xhtml/consulta.xhtml") do |page|
+    search_result = page.form_with(:name =>"filtro") do |search|
+        search.filtro:j_id77 = "DADOSADVOGADO"
+        search.filtro:nomeAdvogado = "João Humberto Martorelli"
+        search.filtro:id117 = ""
+    end.submit
+    
+    search_result.links.each do |link|
+        puts link.text
+    end
+end
+ 
+
+
+
+
+
+
+
+=begin
 # Testando formulario para consulta de processos no TJPE
+
+
 
 # Só fazer o .parser quando for pegar conteudo
 
@@ -16,18 +44,37 @@ page = page.link_with(:text => 'Acesso ao sistema').click
 
 puts page.uri
 
-#pp page.forms.first
+
+pp page.forms.first.fields[-3].value = "DADOSADVOGADO"
+pp page.forms.first.fields[6].value = "João Humberto Martorelli"
+## quebrar o captcha
+pp page.forms.first.fields[-5].value = ""
+
+pp page.forms.first.fields
+
+=end
+
+=begin
+info_search = [] # conjunto de dados para pesquisar
 
 info = page.parser
-info = info.css('form#filtro div.filtro')
+info = info.css('form#filtro div.filtro select.select-tipoConsulta').value
 
 puts info
 
+form = page.forms.first
 
-# Alterar value de cada campo em um variavel diferente
-# Vê como quebrar o captacha
-# Clicar no Button
-# Pegar o resultado da pesquisa
-# Listar os 10 primeiros resultados
+form[:name => "filtro:j_id77"].value = "DADOSADVOGADO"
+
+
+puts "++++++++++++++"
+
+info = page.parser
+info = info.css('form#filtro div.filtro select.select-tipoConsulta').text
+puts info
+
+=end
+
+
 
 
---
+++ introducao-crawler/google.rb
@@ -0,0 +1,18 @@
+require 'mechanize'
+
+a = Mechanize.new { |agent|
+  agent.user_agent_alias = 'Mac Safari'
+}
+
+a.get('http://google.com/') do |page|
+  search_result = page.form_with(:name => 'f') do |search|
+    search.q = 'Hello world'
+  end.submit
+
+  search_result.links.each do |link|
+    puts link.text
+  end
+end
+
+page = a.get("http://google.com/")
+pp page
